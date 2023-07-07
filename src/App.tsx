61c59774a4a807457a09956db54755b5710396ac
devam-Scaletech <devam.chaudhari@scaletech.xyz>
2023-07-07 15:38:32 +0530

    cont: create expanse form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes, Navigate, BrowserRouter } from 'react-router-dom';
 import Layout from 'shared/hoc/layout/component/layout';
+
+import CreateExpense from 'feature/createExpense/container/createExpense';
 import 'assets/styles/index.scss';
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
 			<Layout>
 				<Routes>
 					<Route path='*' element={<Navigate replace to='/homePage' />} />
+					<Route path='/createExpense' element={<CreateExpense />} />
 				</Routes>
 			</Layout>
 		</BrowserRouter>
---
+++ src/feature/createExpense/component/createExpenseForm.tsx
@@ -0,0 +1,7 @@
+const CreateExpenseForm = () => {
+    return (
+        <div>createExpenseForm</div>
+    );
+};
+
+export default CreateExpenseForm;
