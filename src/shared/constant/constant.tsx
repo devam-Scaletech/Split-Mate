ccdc7d48be8ae4f90187cd668ab38d54ca8da2cc
devam-Scaletech <devam.chaudhari@scaletech.xyz>
2023-07-07 12:10:24 +0530

    feat: set data in localStorage

---
+++ src/shared/constant/constant.tsx
@@ -0,0 +1,30 @@
+export const userData = {
+    image: "friendImageOne",
+    userName: "John Doe",
+    mobileNo: "1234567890",
+    friend: [{
+        image: "friendImageTwo",
+        name: "Jane Smith",
+        mobileNo: "9876543210"
+    }, {
+        image: "friendImageThree",
+        name: "Emily Johnson",
+        mobileNo: "9876543210"
+    }, {
+        image: "friendImageFour",
+        name: "David Anderson",
+        mobileNo: "9876543210"
+    }, {
+        image: "friendImageFive",
+        name: "Sarah Thompson",
+        mobileNo: "9876543210"
+    }, {
+        image: "friendImageSix",
+        name: "Michael Wilson",
+        mobileNo: "9876543210"
+    }, {
+        image: "friendImageOne",
+        name: "Olivia Davis",
+        mobileNo: "9876543210"
+    }]
+};
--- src/shared/hoc/layout/component/layout.tsx
+++ src/shared/hoc/layout/component/layout.tsx
@@ -1,10 +1,16 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { TopHeader } from 'shared/component/navigation/topHeader';
+import { userData } from 'shared/constant/constant';
 
 interface IParentComponentProps {
 	children: ReactNode;
 }
 const Layout: React.FC<IParentComponentProps> = (props) => {
+	const user = userData;
+	useEffect(() => {
+		localStorage.setItem('userData', JSON.stringify(user));
+	}, [user]);
+
 	return (
 		<div>
 			{<TopHeader />}
